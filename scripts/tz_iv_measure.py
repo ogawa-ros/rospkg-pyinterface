c1c4a921150a8463ae4f3a6e3fa78f9ff852d26d
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-06-27 01:20:32 +0900

    add

--- scripts/tz_iv_measure.py
+++ scripts/tz_iv_measure.py
@@ -10,30 +10,11 @@ from std_msgs.msg import Int32
 class sis_iv(object):
     def __init__(self):
 
-        self.pub_vol_ch1 = rospy.Publisher("/dev/pci3346A/rsw0/ch1", Float64, queue_size=1)
-        self.pub_vol_ch2 = rospy.Publisher("/dev/pci3346A/rsw0/ch1", Float64, queue_size=1)
-        self.pub_vol_ch3 = rospy.Publisher("/dev/pci3346A/rsw0/ch1", Float64, queue_size=1)
-        self.pub_vol_ch4 = rospy.Publisher("/dev/pci3346A/rsw0/ch1", Float64, queue_size=1)
         self.pub_vol_ch_all = rospy.Publisher("/necst/rx_sis2sb/vgap_cmd", Float64, queue_size=1)
+        #self.pub_rate = rospy.Publisher("/dev/pyinterface", Float64, queue_size=1)
+
 
     def measure(self, initv, interval, repeat):
-        da_all = []
-        self.pub_vol_ch1.publish(initv)
-        self.pub_vol_ch2.publish(initv)
-        self.pub_vol_ch3.publish(initv)
-        self.pub_vol_ch4.publish(initv)
-        time.sleep(0.3)
-        for i in range(repeat+1):
-            da = []
-            vol = initv+interval*i
-            msg = Float64()
-            msg.data = vol
-            self.pub_vol_ch1.publish(msg)
-            self.pub_vol_ch2.publish(msg)
-            self.pub_vol_ch3.publish(msg)
-            self.pub_vol_ch4.publish(msg)
-
-    def measure2(self, initv, interval, repeat):
         da_all = []
         self.pub_vol_ch_all.publish(initv)
         time.sleep(0.3)
@@ -61,7 +42,7 @@ if __name__ == "__main__" :
 
     #repeat = int((lastv-initv)/interval)
     repeat_vgap = int(abs((last_vgap-init_vgap)/interval_vgap))
-    sys.exit(iv.measure2(init_vgap,interval_vgap,repeat_vgap))
+    sys.exit(iv.measure(init_vgap,interval_vgap,repeat_vgap))
 
 #20181204
 #change for tz
