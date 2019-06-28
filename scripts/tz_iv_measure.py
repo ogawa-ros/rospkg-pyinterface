ea702ae0c9308ec8a6a78054a4fd30a38b12db11
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-06-28 13:06:56 +0900

    add

--- scripts/tz_iv_measure.py
+++ scripts/tz_iv_measure.py
@@ -10,8 +10,8 @@ from std_msgs.msg import Int32
 class sis_iv(object):
     def __init__(self):
 
-        self.pub_vol_ch_all = rospy.Publisher("/necst/rx_sis2sb/vgap_cmd", Float64, queue_size=1)
-        #self.pub_vol_ch_all = rospy.Publisher("/tz2019/sis_v1/vgap_cmd", Float64, queue_size=1)
+        #self.pub_vol_ch_all = rospy.Publisher("/necst/rx_sis2sb/vgap_cmd", Float64, queue_size=1)
+        self.pub_vol_ch_all = rospy.Publisher("/tz2019/sis_v1/vgap_cmd", Float64, queue_size=1)
 
         self.pub_path = rospy.Publisher("/logger_path", String, queue_size=1)
         self.pub_rate = rospy.Publisher("/dev/pci3177/rsw0/pub_rate", Float64, queue_size=1)
@@ -48,7 +48,7 @@ if __name__ == "__main__" :
     interval_vgap = float(input("interval_Vgap = ? [Vgap]"))
     save_name = str(input("savename = ? "))
     iv = sis_iv()
-    iv.set()
+    #iv.set()
     #repeat = int((lastv-initv)/interval)
     repeat_vgap = int(abs((last_vgap-init_vgap)/interval_vgap))
     sys.exit(iv.measure(init_vgap,interval_vgap,repeat_vgap))
