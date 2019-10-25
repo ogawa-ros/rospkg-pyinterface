83a0933b96040be726f59facd6e3d3eb0b1b1009
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2019-10-25 17:52:29 +0900

    bug fix

--- scripts/cpz7415v.py
+++ scripts/cpz7415v.py
@@ -273,13 +273,14 @@ if __name__ == '__main__':
         p['mode'] = rospy.get_param('~{ax}_mode'.format(**locals()), default_mode)
 
         mp = {}
-        mp['clock'] = rospy.get_param('~{ax}_clock'.format(**locals()), default_clock)
-        mp['acc_mode'] = rospy.get_param('~{ax}_acc_mode'.format(**locals()), default_acc_mode)
-        mp['low_speed'] = rospy.get_param('~{ax}_low_speed'.format(**locals()), default_low_speed)
-        mp['speed'] = rospy.get_param('~{ax}_speed'.format(**locals()), default_speed)
-        mp['acc'] = rospy.get_param('~{ax}_acc'.format(**locals()), default_acc)
-        mp['dec'] = rospy.get_param('~{ax}_dec'.format(**locals()), default_dec)
-        mp['step'] = rospy.get_param('~{ax}_step'.format(**locals()), default_step)
+        mp[ax] = {}
+        mp[ax]['clock'] = rospy.get_param('~{ax}_clock'.format(**locals()), default_clock)
+        mp[ax]['acc_mode'] = rospy.get_param('~{ax}_acc_mode'.format(**locals()), default_acc_mode)
+        mp[ax]['low_speed'] = rospy.get_param('~{ax}_low_speed'.format(**locals()), default_low_speed)
+        mp[ax]['speed'] = rospy.get_param('~{ax}_speed'.format(**locals()), default_speed)
+        mp[ax]['acc'] = rospy.get_param('~{ax}_acc'.format(**locals()), default_acc)
+        mp[ax]['dec'] = rospy.get_param('~{ax}_dec'.format(**locals()), default_dec)
+        mp[ax]['step'] = rospy.get_param('~{ax}_step'.format(**locals()), default_step)
         p['motion'] = mp
         params.append(p)
         continue
