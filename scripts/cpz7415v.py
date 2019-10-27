930cce109b65b7dce4d14b5e452fc64609f25562
s7u27g <s7u27astr0@gmail.com>
2019-10-27 16:56:01 +0900

    bug fix

--- scripts/cpz7415v.py
+++ scripts/cpz7415v.py
@@ -31,6 +31,7 @@ class cpz7415v_controller(object):
     execute_params_lock = False
     last_mode = {}
     status = {}
+    last_direction = 0
 
     def __init__(self, rsw_id, params):
         self.rsw_id = rsw_id
@@ -167,8 +168,7 @@ class cpz7415v_controller(object):
                     pass
 
             elif self.params[axis]['mode'] == 'jog':
-                last_direction = self.params[axis]['motion'][axis]['step']
-                if (last_direction * param > 0) & (is_moving(axis)):
+                if (self.last_direction * param > 0) & (is_moving(axis)):
                     self.mot.change_speed(axis=axis, mode='accdec_change', speed=[abs(param)])
                 else:
                     stop_move(axis)
@@ -180,6 +180,7 @@ class cpz7415v_controller(object):
                     self.params[axis]['motion'][axis]['speed'] = abs(param)
                     set_start(axis)
                     pass
+                self.last_direction = self.params[axis]['motion'][axis]['step']
                 pass
 
         elif type_ == 'step':
