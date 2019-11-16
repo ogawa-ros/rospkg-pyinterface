da37e627ca58ff5c01691fccbacfe461a9fcaf0b
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2019-11-16 23:11:45 +0900

    bug fix

--- scripts/pci7415_driver.py
+++ scripts/pci7415_driver.py
@@ -69,7 +69,6 @@ class pci7415_driver(object):
                 continue
             # 所要時間を測る
             t2 = time.time()
-            print('dt1: {0:.6f}, dt2: {1:.6f}'.format(t1-t0, t2-t1))
             self.pub_dt1.publish(t1-t0)
             self.pub_dt2.publish(t2-t1)
             if not self.func_queue.empty():
@@ -123,7 +122,7 @@ class pci7415_driver(object):
     def oputput_do(self, data, axis):
         self.mot.output_do(data)
         pass
-        
+
     def start(self, data, axis):
         self.mot.set_motion(axis=axis, mode=self.mode, motion=self.motion)
         self.mot.start_motion(axis=axis, start_mode='acc', move_mode=self.params[axis]['mode'])
