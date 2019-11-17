cf477a98369729ef0cebc7ffe9f3a3a98a90230e
s7u27g <s7u27astr0@gmail.com>
2019-11-18 00:38:07 +0900

    bug fix

--- scripts/pci7415_handler.py
+++ scripts/pci7415_handler.py
@@ -55,7 +55,7 @@ class pci7415_handler(object):
             return
 
         if self.move_mode[ax] == 'jog':
-            if (self.last_speed[axis] * param > 0) & (self.current_speed[ax] != 0):
+            if (self.last_speed[ax] * param > 0) & (self.current_speed[ax] != 0):
                 #pub change_speed
                 self.pub[ax+'_change_speed'].publish(abs(speed.data))
                 pass
