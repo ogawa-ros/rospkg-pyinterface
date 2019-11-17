4aa89e7c69df19bd07d00a3cc170b2f037969e28
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2019-11-17 20:51:57 +0900

    test

--- experiment/change_direction.py
+++ experiment/change_direction.py
@@ -71,42 +71,21 @@ pub[use_axis]['set_speed'].publish(speed)
 pub[use_axis]['set_acc'].publish(acc)
 pub[use_axis]['set_dec'].publish(dec)
 
-for i in range(3):
+for i in range(8):
     pub[use_axis]['set_step'].publish(step1)
-    pub[use_axis]['start'].publish(1)
-    time.sleep(5)
-    pub[use_axis]['stop'].publish(1)
-    time.sleep(5)
-
-    pub[use_axis]['set_step'].publish(step1)
+    time.sleep(1e-3)
     pub[use_axis]['start'].publish(1)
     time.sleep(5)
     pub[use_axis]['stop'].publish(1)
     time.sleep(5)
 
     pub[use_axis]['set_step'].publish(step1)
+    time.sleep(1e-3)
     pub[use_axis]['start'].publish(1)
     time.sleep(5)
     pub[use_axis]['stop'].publish(1)
     time.sleep(5)
 
-    pub[use_axis]['set_step'].publish(step2)
-    pub[use_axis]['start'].publish(1)
-    time.sleep(5)
-    pub[use_axis]['stop'].publish(1)
-    time.sleep(5)
-
-    pub[use_axis]['set_step'].publish(step2)
-    pub[use_axis]['start'].publish(1)
-    time.sleep(5)
-    pub[use_axis]['stop'].publish(1)
-    time.sleep(5)
-
-    pub[use_axis]['set_step'].publish(step2)
-    pub[use_axis]['start'].publish(1)
-    time.sleep(5)
-    pub[use_axis]['stop'].publish(1)
-    time.sleep(5)
     continue
 
 logger.stop()
