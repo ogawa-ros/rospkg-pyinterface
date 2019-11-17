b9724a34912e8c40a4000b336c55c7e98f5beb23
s7u27g <s7u27astr0@gmail.com>
2019-11-18 01:00:03 +0900

    bug fix

--- experiment/handler_speed0.py
+++ experiment/handler_speed0.py
@@ -32,9 +32,8 @@ for ax in use_axis:
     pub[ax] = {}
     pub[ax]['step'] = rospy.Publisher(b+'step_cmd', std_msgs.msg.Int64, queue_size=1)
     pub[ax]['speed'] = rospy.Publisher(b+'speed_cmd', std_msgs.msg.Float64, queue_size=1)
-pub_outputdo =rospy.Publisher(base+'/output_do', std_msgs.msg.Int64MultiArray, queue_size=1)
-
     time.sleep(0.1)
+pub_outputdo =rospy.Publisher(base+'/output_do', std_msgs.msg.Int64MultiArray, queue_size=1)
 
 logger.start(file_name)
 conf = std_msgs.msg.Int64MultiArray()
